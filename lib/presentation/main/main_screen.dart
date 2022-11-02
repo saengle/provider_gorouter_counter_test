9951f6221a0e501d15a706b72474dd810e63f738
saengle <zohv817@gmail.com>
2022-11-02 15:24:24 +0900

    카운터 기본 상태

---
+++ lib/presentation/main/main_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              counter++;
+            });
+          },
+          child: Text(
+            style: const TextStyle(fontSize: 80),
+            '$counter',
+          ),
+        ),
+      ),
+    );
+  }
+}
